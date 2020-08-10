61b58d3e95ce69a1cfd04367321a0cdd865617ac
M1sAel <69442079+M1sAel@users.noreply.github.com>
2020-08-10 03:18:25 -0300

    Create menu.py

---
+++ menu/menu.py
@@ -0,0 +1,21 @@
+def main():
+    # cores
+    az = '\033[1;36m'
+    ver = '\033[32;6m'
+    ama = '\033[1;33m'
+    fnl = '\033[m'
+    ng = '\033[1m'
+    print(f'''
+    	
+  {ama}[+] SISTEMA DE CADRASTO [+]{fnl}
+  {ver}
+  cadastre-se para receber o livro  
+  do WordPress do estado de São Paulo 
+  lenbrando que os livros serão entrgue
+  na data 02/07 os livros serão entregues
+  em casa não perga essa oportunidade
+  {fnl}
+  {az}[{fnl}{ama}1{fnl}{az}]{fnl} {ng}Cadrasta{fnl}
+  {az}[{fnl}{ama}2{fnl}{az}]{fnl} {ng}Sair{fnl}
+          ''')
+
